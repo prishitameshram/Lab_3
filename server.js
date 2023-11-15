f58e4d25490ef0630c45f494b0198c90b60dd428
prishitameshram <prishitameshram@gmail.com>
2023-11-14 20:04:15 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors()); // Adding this line to enable CORS
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/files', (req, res) => {
+  const files = fs.readdirSync('uploads/');
+  res.json({ files });
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ message: 'File uploaded successfully.' });
+});
+
+app.delete('/delete/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join('uploads/', filename);
+
+  try {
+    fs.unlinkSync(filePath);
+    res.json({ message: 'File deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
